2fee1bf69b235f1125d9842dc54aada2f0b636ef
Deborah Imaji <101178292+DeborahImaji@users.noreply.github.com>
2025-08-01 17:01:01 +0100

    Began writing code in chat.tsx

---
+++ app/(tabs)/chat/chat.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const ChatPage = () => {
+    return (
+        <View>
+            <Text>This is the chat page</Text>
+        </View>
+    );
+};
