e985b5937dc88fcefec9d8852b0e81eb63c8aa9d
Deborah Imaji <101178292+DeborahImaji@users.noreply.github.com>
2025-08-04 17:01:33 +0100

    Completed the header title

--- app/(tabs)/chat/chat.tsx
+++ app/(tabs)/chat/chat.tsx
@@ -1,3 +1,4 @@
+import { MaterialIcons } from '@expo/vector-icons';
 import MaskedView from '@react-native-masked-view/masked-view';
 import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
@@ -7,13 +8,18 @@ const ChatPage = () => {
     return (
         <ScrollView style={styles.container}>
             <View style={styles.header}>
-                <MaskedView maskElement={<Text style={styles.headerTitle}>Caskayd</Text>}>
+                <MaskedView style={styles.maskedViewContainer} maskElement={<Text style={styles.headerTitle}>Caskayd</Text>}>
                     <LinearGradient
                         colors={['#ad8995', '#a44576', '#998a87']}
                         start={{ x: 0, y: 0 }}
                         end={{ x: 1, y: 0 }}
+                        style={StyleSheet.absoluteFill}
                     />
                 </MaskedView>
+                <View style={styles.headerIcons}>
+                    <MaterialIcons name="search" size={28} color="#555" style={styles.headerIcon} />
+                    <MaterialIcons name="more-vert" size={28} color="#555" style={styles.headerIcon} />
+                </View>
             </View>
         </ScrollView>
     );
@@ -29,9 +35,20 @@ const styles = StyleSheet.create({
     header: {
         paddingTop: 28,
     },
+    maskedViewContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        height: 100,
+    },
     headerTitle: {
         fontSize: 32,
         fontWeight: 'bold',
+    },
+    headerIcons: {
+        flexDirection: 'row',
+    },
+    headerIcon: {
+        marginLeft: 15,
     }});
 
 export default ChatPage;
